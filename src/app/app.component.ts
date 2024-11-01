a1fa39fbebfe523afd0a22237d180f5d382e259b
Caio Vinícius Rodrigues Moreira <you@example.com>
2024-11-01 12:56:17 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tripon-passagens-hospedanges-frontend';
+  title = 'tripon-passagens-hospedagens-frontend';
 }
